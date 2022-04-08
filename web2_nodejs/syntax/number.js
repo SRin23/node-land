062d0fcf3111f277fd04192d1b80a6fe93b024d7
SRin23 <serin4723@naver.com>
2022-04-08 22:31:17 +0900

    [Javascript] Data type - Number #2

---
+++ web2_nodejs/syntax/number.js
@@ -0,0 +1,18 @@
+//Javascript - Number
+
+//숫자 출력 방법
+console.log(1)
+
+//사칙연산
+
+//+(더하기) : 이항연산자로 숫자를 더함
+console.log(1+1)
+
+//-(빼기) : 숫자를 뺌
+console.log(3-1);
+
+//*(곱하기) : 수를 곱함
+console.log(2*2)
+
+// /(나누기) : 수를 나눔
+console.log(10/2)
