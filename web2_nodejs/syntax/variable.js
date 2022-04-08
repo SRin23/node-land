3d15223ec02475944520c1ab09b4ef80d67873a5
SRin23 <serin4723@naver.com>
2022-04-08 23:38:58 +0900

    [Javascript] 변수 #2

---
+++ web2_nodejs/syntax/variable.js
@@ -0,0 +1,12 @@
+//Javascript - 변수의 형식
+
+//변수 = 상수로 변수에 상수 대입
+//변수 선언 : var
+var a = 1;
+console.log(a);
+
+a = 7;
+console.log(a);
+
+//상수에 상수를 대입하는건 안됨
+//1 = 2
---
+++ web2_nodejs/syntax/variable2.js
@@ -0,0 +1,7 @@
+//Javascript - 변수의 활용
+
+var name = 'Serin'
+
+var letter = 'Dear ' + name + ' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, ' + name + ' quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse ' + name + ' cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  ' + name + ' ';
+
+console.log(letter);
