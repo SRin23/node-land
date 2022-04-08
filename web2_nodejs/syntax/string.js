9af3f0ec7f544900716054e972db1cfe5f844e0b
SRin23 <serin4723@naver.com>
2022-04-08 22:35:29 +0900

    [Javascript] Data type - String

---
+++ web2_nodejs/syntax/string.js
@@ -0,0 +1,9 @@
+//Javascript - Data type - string
+
+//String 표현 방법 : 큰따옴표 - 큰따옴표 or 작은따옴표 - 작은따옴표
+
+//숫자가 아닌 문자이므로 2가 아닌, 11이됨(결합)
+console.log('1' + '1')  //11
+
+//.length - 문자열의 길이 세기
+console.log('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'.length);   //446
