515286aaa59a7f0840ad6439d5ab4ec298dcd4dc
SRin23 <serin4723@naver.com>
2022-04-11 20:20:01 +0900

    [Node] URL로 입력된 값 사용하기 #3

--- web2_nodejs/main.js
+++ web2_nodejs/main.js
@@ -1,17 +1,26 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url'); //url module 사용
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    var _url = request.url;
+
+    var queryData = url.parse(_url, true).query;
+    console.log(queryData.id);
+    console.log(_url);
+
+    if(_url == '/'){
+      _url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
+
+    if(_url == '/favicon.ico'){
         response.writeHead(404);
         response.end();
         return;
     }
+
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
- 
+    //response.end(fs.readFileSync(__dirname + _url));  //사용자가 접속한 url에 따라서 그 파일을 읽어오는것
+    response.end(queryData.id);  //queryData.id를 화면에 출력시킴
 });
 app.listen(3000);
