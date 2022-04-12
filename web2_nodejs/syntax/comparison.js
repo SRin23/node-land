4785fc27871c4f214806a4e091f5f497b1cad1c7
SRin23 <serin4723@naver.com>
2022-04-12 22:10:32 +0900

    [Javascript] 비교연산자 #4

---
+++ web2_nodejs/syntax/comparison.js
@@ -0,0 +1,13 @@
+//Comparison 비교연산자
+console.log(1==1)   //1과 1이 같은가? - true
+console.log(1==2)   //1과 2가 같은가 - false
+
+console.log(1>2)    //1이 2보다 큰가? - false
+console.log(1<2)    //1이 2보다 작은가? - true
+
+console.log(1===1)  //1과 1이 정확히 같은가 - true
+console.log(1===2)  //1과 2가 정확히 같은가 - false
+
+
+// = 는 대입연산자
+// == 는 비교연산자로 다른 의미를 가진다.
