22c567cf6859ec6fcd9542a0d98073418db4c9b8
SRin23 <serin4723@naver.com>
2022-04-14 19:37:10 +0900

    [Javascript] 함수의 출력 #6

--- web2_nodejs/syntax/function2.js
+++ web2_nodejs/syntax/function2.js
@@ -9,5 +9,9 @@ console.log(Math.round(1.4));   //1
 sum(2, 4);  //argument
 
 function sum(first, second){    //매개변수(parameter)
-    console.log(first + second);
+    console.log('A');
+    return first + second;  //즉시 함수 실행 종료 -> 호출한 곳에 first + second의 값을 반환함
+    console.log('B')    //실행되지 않음
 }
+
+console.log(sum(2, 4));
